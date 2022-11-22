95bd765f822b6396b6077ed6b218c63112e55313
Abdulla Sirajudeen <abdullasirajudeen@gmail.com>
2022-11-22 16:36:41 +0400

    Add project files.

---
+++ LoggingApplicationBlock/Executer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace LoggingApplicationBlock
+{
+    class Executer
+    {
+        LoggerBlock loggerBlock = new LoggerBlock();
+
+        public static void Main()
+        {
+            new Executer().ReadFile();
+        }
+
+        public void ReadFile()
+        {
+            WriteTraceLog("Application Started!!!");
+
+            string[] lines;
+            var list = new List<string>();
+            var fileStream = new FileStream(@"sample.txt", FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    WriteTraceLog(line);
+
+                    list.Add(line);
+                }
+            }
+
+            lines = list.ToArray();
+
+            WriteTraceLog("Application Stopped!!!");
+        }
+
+        public void WriteTraceLog(String message)
+        {
+
+            Logger.Write(message, "General", 5, 2000, TraceEventType.Information);
+        }
+    }
+}
---
+++ LoggingApplicationBlock/LoggerBlock.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+
+namespace LoggingApplicationBlock
+{
+    public class LoggerBlock
+    {
+        protected LogWriter logWriter;
+
+        public LoggerBlock()
+        {
+            InitLogging();
+        }
+
+        private void InitLogging()
+        {
+            IConfigurationSource configurationSource = ConfigurationSourceFactory.Create();
+            //LogWriterFactory logWriterFactory = new LogWriterFactory(configurationSource);
+            //Logger.SetLogWriter(logWriterFactory.Create(), false);
+            logWriter = new LogWriterFactory(configurationSource).Create();
+            //  Logger.SetLogWriter(logWriter, false);
+            Logger.SetLogWriter(logWriter, false);
+
+
+        }
+
+        public LogWriter LogWriter
+        {
+            get
+            {
+                return logWriter;
+            }
+        }
+    }
+}
